a6cd2c81d0a2b3927ecf94a67b4c556f473782e8
Greg Ross <greg@toolstack.com>
2015-01-27 20:28:35 -0500

    - Avoid opening javascript buttons in a new window.

--- plugins/new-window-for-external-links/new-window-for-external-links.js
+++ plugins/new-window-for-external-links/new-window-for-external-links.js
@@ -2,7 +2,7 @@ jQuery(document).ready( function() {
 	var wefel_root = location.protocol + '//' + location.host;
 
 	jQuery('a').each( function() {
-		if( this.href.indexOf( wefel_root ) == -1 )
+		if( this.href.indexOf( wefel_root ) == -1 && this.href.indexOf( 'javascript') == -1 )
 			{
 			this.target = "_blank";
 			}
